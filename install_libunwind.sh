f52a067824022bce06178add49185532f63c4b9a
Marcin Copik <mcopik@gmail.com>
2020-02-15 11:00:36 +0100

    Add Docker image for libunwind

---
+++ install_libunwind.sh
@@ -0,0 +1,23 @@
+VERSION=$1
+
+wget http://releases.llvm.org/${VERSION}/libunwind-${VERSION}.src.tar.xz
+tar -xf libunwind-${VERSION}.src.tar.xz && rm libunwind-${VERSION}.src.tar.xz
+mv libunwind-${VERSION}.src libunwind
+
+mkdir build_libunwind
+cd build_libunwind
+
+FLAGS="-fsanitize=dataflow -fsanitize-blacklist=/dfsan_abilist.txt"
+
+cmake -G "Ninja"\
+  -DCMAKE_BUILD_TYPE=MinSizeRel\
+  -DCMAKE_INSTALL_PREFIX=/opt/llvm\
+  -DCMAKE_C_COMPILER=clang\
+  -DCMAKE_CXX_COMPILER=clang++\
+  -DLLVM_PATH=/opt/llvm/lib\
+  -DCMAKE_C_FLAGS="$FLAGS"\
+  -DCMAKE_CXX_FLAGS="$FLAGS"\
+  -DLIBUNWIND_ENABLE_SHARED=NO\
+  ../libunwind
+ninja -j${NCPUS} install
+
