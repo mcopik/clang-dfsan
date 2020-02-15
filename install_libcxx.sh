ed1b8956628a2fbc183564df86b06ba808a21e53
Marcin Copik <mcopik@gmail.com>
2020-02-15 10:17:57 +0100

    Update Docker files for llvm & libcxx

---
+++ install_libcxx.sh
@@ -0,0 +1,45 @@
+VERSION=$1
+
+wget http://releases.llvm.org/${VERSION}/libcxx-${VERSION}.src.tar.xz
+wget http://releases.llvm.org/${VERSION}/libcxxabi-${VERSION}.src.tar.xz
+tar -xf libcxx-${VERSION}.src.tar.xz && rm libcxx-${VERSION}.src.tar.xz
+tar -xf libcxxabi-${VERSION}.src.tar.xz && rm libcxxabi-${VERSION}.src.tar.xz
+mv libcxx-${VERSION}.src libcxx
+mv libcxxabi-${VERSION}.src libcxxabi
+
+mkdir build_libcxxabi
+cd build_libcxxabi
+
+cmake -G "Ninja"\
+  -DCMAKE_BUILD_TYPE=MinSizeRel\
+  -DCMAKE_INSTALL_PREFIX=/opt/llvm\
+  -DCMAKE_C_COMPILER=clang\
+  -DCMAKE_CXX_COMPILER=clang++\
+  -DLLVM_PATH=/opt/llvm/lib\
+  -DCMAKE_C_FLAGS=-fsanitize=dataflow\
+  -DCMAKE_CXX_FLAGS=-fsanitize=dataflow\
+  -DLIBCXXABI_ENABLE_SHARED=NO\
+  -DLIBCXXABI_LIBCXX_PATH=../libcxx\
+  ../libcxxabi
+ninja -j${NCPUS} install
+
+cd ..
+
+mkdir build_libcxx
+cd build_libcxx
+
+cmake -G "Ninja"\
+  -DCMAKE_BUILD_TYPE=MinSizeRel\
+  -DCMAKE_INSTALL_PREFIX=/opt/llvm\
+  -DCMAKE_C_COMPILER=clang\
+  -DCMAKE_CXX_COMPILER=clang++\
+  -DCMAKE_C_FLAGS=-fsanitize=dataflow\
+  -DCMAKE_CXX_FLAGS=-fsanitize=dataflow\
+  -DLIBCXX_ENABLE_SHARED=OFF\
+  -DLIBCXX_CXX_ABI=libcxxabi\
+  -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON\
+  -DLIBCXX_CXX_ABI_INCLUDE_PATHS=../libcxxabi/include/\
+  -DLIBCXX_CXX_ABI_LIBRARY_PATH=../build_libcxxabi/lib/\
+  ../libcxx
+ninja -j${NCPUS} install
+
---
+++ install_llvm.sh
@@ -0,0 +1,25 @@
+VERSION=$1
+
+# Download sources for LLVM, Clang and compiler-rt
+wget http://releases.llvm.org/${VERSION}/llvm-${VERSION}.src.tar.xz
+wget http://releases.llvm.org/${VERSION}/cfe-${VERSION}.src.tar.xz
+wget http://releases.llvm.org/${VERSION}/compiler-rt-${VERSION}.src.tar.xz
+for archive in *.tar.xz; do
+  tar -xf ${archive}
+done
+mv cfe-${VERSION}.src clang
+mv compiler-rt-${VERSION}.src compiler-rt
+
+mkdir build_llvm
+cd build_llvm
+cmake -G "Ninja"\
+  -DCMAKE_BUILD_TYPE=MinSizeRel\
+  -DLLVM_TARGETS_TO_BUILD="X86"\
+  -DLLVM_ENABLE_PROJECTS="clang;compiler-rt;lld;libunwind"\
+  -DLLVM_INCLUDE_TESTS=Off\
+  -DLLVM_INCLUDE_BENCHMARKS=Off\
+  -DCMAKE_INSTALL_PREFIX=/opt/llvm/\
+  ../llvm-${VERSION}.src
+
+ninja -j${NCPUS} install
+
